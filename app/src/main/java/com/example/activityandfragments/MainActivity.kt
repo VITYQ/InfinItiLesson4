72a67e60b0d6abdcf393961ec0422d09d241c64d
VITYQ <vitaly.sorokin14@gmail.com>
2021-10-16 13:00:14 +0300

    Lesson 2 commit

--- app/src/main/java/com/example/activityandfragments/MainActivity.kt
+++ app/src/main/java/com/example/activityandfragments/MainActivity.kt
@@ -5,20 +5,47 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.Toast
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val mButton = findViewById<Button>(R.id.button)
+        val fragmentManager = supportFragmentManager.beginTransaction()
+        fragmentManager
+            .replace(R.id.container_main, Fragment1())
+            .commit()
 
-        mButton.setOnClickListener {
-//            Toast.makeText(this, "Ура, у меня работает кнопочка", Toast.LENGTH_SHORT).show()
-            val intent = Intent(this, MainActivity2::class.java)
-            startActivity(intent)
+        val mBottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        mBottomNav.setOnItemSelectedListener {
+            val fragmentManager = supportFragmentManager.beginTransaction()
+
+            when(it.itemId){
+                R.id.page1 ->{
+                    fragmentManager
+                        .replace(R.id.container_main, Fragment1())
+                        .commit()
+                    true
+                }
+                R.id.page2 -> {
+                    fragmentManager
+                        .replace(R.id.container_main, Fragment2())
+                        .commit()
+                    true
+                }
+                R.id.page3 -> {
+                    true
+                }
+                else -> false
+            }
         }
+
+
     }
 
 
